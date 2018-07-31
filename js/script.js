0590ce9b18310e16b76c76d512e44deab937d8af
George Tang <gtangg12@gmail.com>
2018-07-31 13:17:47 -0400

    interface

--- js/script.js
+++ js/script.js
@@ -4,12 +4,9 @@ function openPage(pageName, elmnt) {
     content = document.getElementsByClassName("content");
     for (i = 0; i < content.length; i++)
         content[i].style.display = "none";
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-        tablinks[i].style.color = "white";
-    }
     document.getElementById(pageName).style.display = "block";
-    elmnt.style.backgroundColor = "white";
-    elmnt.style.color = "#0A2E5E";
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++)
+        tablinks[i].style.color = "#9A9A9A";
+    elmnt.style.color = "white";
 }
