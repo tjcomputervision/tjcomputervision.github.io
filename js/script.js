296709e8a4f1ec07b64ea58e284ea1a834f3c585
George Tang <gtangg12@gmail.com>
2018-09-08 12:34:53 -0400

    design

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 function openPage(pageName, elmnt) {
 
-    var i, content, tablinks;
+    var i, content, tablinks, dots;
     content = document.getElementsByClassName("content");
-    for (i = 0; i < content.length; i++)
+    dots = document.getElementsByClassName("dot");
+    for (i = 0; i < content.length; i++) {
         content[i].style.display = "none";
+        dots[i].style.background = "#FFF";
+    }
     document.getElementById(pageName).style.display = "block";
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++)
         tablinks[i].style.color = "#9a9a9a";
     elmnt.style.color = "white";
+    document.getElementById("d"+pageName).style.background = "#1CB0F5";
     document.body.scrollTop = document.documentElement.scrollTop = 0;
 }
